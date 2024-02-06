bd3e99834b8667df4765da1d9ee0d61a4c4d97cf
Tyler Hoyt <trh5303@gmail.com>
2024-02-05 19:13:51 -0600

    Should be able to work with a small version now.

--- lua/notepad/init.lua
+++ lua/notepad/init.lua
@@ -2,17 +2,16 @@ local path = require("plenary.path")
 local popup = require("plenary.popup")
 
 local dir_path = vim.fn.stdpath("data") .. "/notes"
-local notepad_path = dir_path .. "/notepad.md"
 
-function basename()
-    local base_name = vim.fs.basename(vim.fn.getcwd())
-    local parent_base_name = vim.fs.basename(vim.fs.dirname(vim.fn.getcwd()))
-    print(parent_base_name .. '_' .. base_name ..'.md')
+-- Get the parent directory of the current working directory
+local function get_parent_dir()
+    local parent_dir = vim.fs.basename(vim.fs.dirname(vim.fn.getcwd()))
+    return parent_dir
 end
 
 -- Need to check if notepad exists for the current project
-function get_info()
-    print(vim.api.nvim_buf_get_name(0))
+local function set_dir_path()
+    local notepad_path = dir_path .. get_parent_dir() .. "/notepad.md"
 
     if vim.fn.isdirectory(dir_path) == 0 then
         print("Notes directory does not exist")
@@ -29,55 +28,34 @@ function get_info()
     end
 end
 
-local M = {}
+local function create_notepad()
+    -- Setup default window
+    local height = 20
+    local width = 80
+    local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+    local bufnr = vim.api.nvim_create_buf(false, false)
 
-function M.setup()
-    get_info()
+    vim.api.nvim_buf_set_name(bufnr, get_parent_dir() .."/notepad.md")
+
+    local win_id = popup.create(bufnr, {
+        title = "Notepad",
+        highlight = "MyNotepadWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        style = "minimal",
+        borderchars = borderchars,
+    })
 end
 
 
--- function create_notepad()
---     -- Setup default window
---     local height = 20
---     local width = 80
---     local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
---     local bufnr = vim.api.nvim_create_buf(false, false)
--- 
---     local win_id = popup.create(bufnr, {
---         title = "Notepad",
---         highlight = "MyNotepadWindow",
---         line = math.floor(((vim.o.lines - height) / 2) - 1),
---         col = math.floor((vim.o.columns - width) / 2),
---         minwidth = width,
---         minheight = height,
---         style = "minimal",
---         borderchars = borderchars,
---     })
--- 
---     setup_autocmds_and_keymaps(bufnr)
--- end
--- 
--- -- this should let us run our code on buffer save
--- function setup_autocmds_and_keymaps(bufnr)
---     local curr_file = vim.api.nvim_buf_get_name(0)
--- 
---     vim.api.nvim_create_autocmd({ "BufLeave", "BufWriteCmd" }, {
---         buffer = bufnr,
---         callback = function()
---             -- get_lines()
---         end,
---     })
--- end
--- 
--- function get_lines()
---     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
---     for i in ipairs(lines) do
---         print(lines[i])
---     end
--- end
--- 
--- function write_lines()
--- 
--- end
+local M = {}
+
+function M.setup()
+    -- Sets up our ntoes directory and notepad file for the current project
+    set_dir_path()
+    create_notepad()
+end
 
 return M
--- lua/notepad/something/something.lua
+++
@@ -1,6 +0,0 @@
-
-function basename()
-    local base_name = vim.fs.basename(vim.fn.getcwd())
-    local parent_base_name = vim.fs.basename(vim.fs.dirname(vim.fn.getcwd()))
-    print(parent_base_name .. '_' .. base_name ..'.md')
-end
