162396494fb5cec5f785e5cb4969e4a0b6bcc0fa
Tyler Hoyt <trh5303@gmail.com>
2024-02-06 11:30:10 -0600

    Added changing of filetypes

--- lua/notepad/init.lua
+++ lua/notepad/init.lua
@@ -4,15 +4,15 @@ local popup = require("plenary.popup")
 local dir_path = vim.fn.stdpath("data") .. "/notes"
 
 -- Get the parent directory of the current working directory
-local function get_parent_dir()
+local function get_parent_dir(filetype)
     local base_dir = vim.fs.basename(vim.fn.getcwd())
     local parent_base_dir = vim.fs.basename(vim.fs.dirname(vim.fn.getcwd()))
-    return "/" .. base_dir .. "_" .. parent_base_dir .. ".md"
+    return "/" .. base_dir .. "_" .. parent_base_dir .. filetype
 end
 
 -- Need to check if notepad exists for the current project
-local function set_dir_path()
-    local notepad_path = dir_path .. get_parent_dir()
+local function set_dir_path(filetype)
+    local notepad_path = dir_path .. get_parent_dir(filetype)
 
     if vim.fn.isdirectory(dir_path) == 0 then
         path:new(dir_path):mkdir()
@@ -25,9 +25,20 @@ end
 
 local M = {}
 
-function M.setup()
+function M.setup(opts)
+    opts = opts or {}
+
+    -- Set filetype to .md if not specified
+    if opts.filetype ~= ".md" or opts.filetype ~= ".txt" or opts.filetype ~= ".norg" then
+        error("Notepad: Filetype must be .md or .txt or .norg. Defaulting to .md.")
+        opts.filetype = ".md"
+        return
+    else
+        opts.filetype = opts.filetype
+    end
+
     -- Sets up our notes directory and notepad file for the current project
-    set_dir_path()
+    set_dir_path(opts.filetype)
     vim.api.nvim_set_keymap("n", "<leader>nn", ":lua require('notepad').open_notepad()<CR>", { noremap = true, silent = true })
 end
 
@@ -49,7 +60,8 @@ function M.open_notepad()
         title = "Notepad",
         highlight = "MyNotepadWindow",
         line = math.floor(((vim.o.lines - height) / 2) - 1),
-        col = math.floor((vim.o.columns - width) / 2), minwidth = width,
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
         minheight = height,
         style = "minimal",
         borderchars = borderchars,
